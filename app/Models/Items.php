6dbe10f5d6941fac4848edd8264133047443a259
Falentino Djoka <Falentinodjoka2801@gmail.com>
2024-07-03 12:06:10 +0700

    Update model Item menjadI Items

--- app/Models/Items.php
+++ app/Models/Items.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
-class Item extends Model{
-    protected $table            =   'item';
+class Items extends Model{
+    protected $table            =   'items';
     protected $primaryKey       =   'id';
     protected $keyType          =   'integer';
     public $timestamps          =   false;
@@ -18,22 +18,6 @@ class Item extends Model{
     #Item yang memiliki stock
     public static array $itemsHaveStock     =   [11];
 
-    public static function listKondisi(): array{
-        $listKondisi    =   [
-            ['id' => 'bagus', 'nama' => 'Bagus'],
-            ['id' => 'rusak', 'nama' => 'Rusak']
-        ];
-
-        return $listKondisi;
-    }
-    public static function listStatus(): array{
-        $listStatus    =   [
-            ['id' => 'ready', 'nama' => 'Ready'],
-            ['id' => 'dipinjam', 'nama' => 'Dipinjam']
-        ];
-
-        return $listStatus;
-    }
     public function items(): HasMany{
         return $this->hasMany(ItemDetail::class, 'item', 'id');
     }
