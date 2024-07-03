35f128cd153a49a2544c18c174f32acc34d417f0
Falentino Djoka <Falentinodjoka2801@gmail.com>
2024-07-03 14:24:26 +0700

    Pengembangan model Jenis: Penambahan static member (method, dan property)

--- app/Models/Jenis.php
+++ app/Models/Jenis.php
@@ -9,4 +9,28 @@ class Jenis extends Model{
     protected $primaryKey       =   'id';
     protected $keyType          =   'integer';
     public $timestamps          =   false;
+
+    public static string $jenis_alat        =   '3';
+    public static string $jenis_ruangan     =   '5';
+    public static string $jenis_phantom     =   '10';
+    public static string $jenis_bhp         =   '11';
+
+    public static function getInitialLetter($jenis): string{
+        $initialLetter  =   null;
+        
+        if($jenis == self::$jenis_alat){
+            $initialLetter  =   'A';
+        }
+        if($jenis == self::$jenis_ruangan){
+            $initialLetter  =   'R';
+        }
+        if($jenis == self::$jenis_phantom){
+            $initialLetter  =   'P';
+        }
+        if($jenis == self::$jenis_bhp){
+            $initialLetter  =   'BHP';
+        }
+
+        return $initialLetter;                
+    }
 }
