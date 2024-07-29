ed1add9d458fc3ec9ef2a49df9aa715b7dc912f3
Falentino Djoka <Falentinodjoka2801@gmail.com>
2024-07-29 08:37:24 +0700

    Penambahan fungsionalitas untuk upload foto profil (belum selesai)

--- resources/views/administrator/profile/index.blade.php
+++ resources/views/administrator/profile/index.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
 @php
-    $administratorNama  =   $administrator->nama;
-    $administratorUserName = $administrator->username;
-    $administratorAlamat    =   $administrator->alamat;
-    $administratorEmail = $administrator->email;
-    $administratorTelepon = $administrator->telepon;
-    $administratorCreatedAt = $administrator->createdAt;
-    $administratorFoto = $administrator->foto;
+$administratorNama = $administrator->nama;
+$administratorUserName = $administrator->username;
+$administratorAlamat = $administrator->alamat;
+$administratorEmail = $administrator->email;
+$administratorTelepon = $administrator->telepon;
+$administratorCreatedAt = $administrator->createdAt;
+$administratorFoto = $administrator->foto;
 @endphp
 <div class="container-fluid">
     <div class="row">
@@ -17,10 +17,8 @@
                 <div class="card-body box-profile">
                     <div class="text-center">
                         <label for="uploadFile" data-toggle='tooltip' title='Klik gambar untuk mengupload Foto Profile baru'>
-                            <img class="img-fluid img-circle img-100-100" src="{{(!empty($administratorFoto))? asset('admin_profile_pict/'.$administratorFoto) : ''}}" alt="{{$administratorNama}}"
-                                onError='this.src="{{asset('img/icon.png')}}"' id='fotoProfile'>
-                            <input type="file" id="uploadFile" name='uploadFile' onChange='_imgChanged(this)'
-                                data-preview='#fotoProfile' style='display:none;' />
+                            <img class="img-fluid img-circle img-100-100" src="{{(!empty($administratorFoto))? asset('admin_profile_pict/'.$administratorFoto) : ''}}" alt="{{$administratorNama}}" onError='this.src="{{asset('img/icon.png')}}"' id='fotoProfile'>
+                            <input type="file" id="uploadFile" name='uploadFile' onChange='_imgChanged(this)' data-preview='#fotoProfile' style='display:none;' />
                         </label>
                     </div>
                     <h3 class="profile-username text-center mt-3">{{$administratorNama}}</h3>
@@ -47,47 +45,53 @@
                 <div class="card-body">
                     <div class="tab-content">
                         <div class="active tab-pane" id="profile">
+                            <h5 class='mb-1'>Form Update Profile</h5>
+                            <p class="text-sm text-muted mb-4">Silahkan update profile anda dengan mengisi form di bawah ini</p>
                             <form action="{{route('admin.profile.save')}}" id="formProfile" method="post" onSubmit='_onFormProfilSubmit(this, event)'>
                                 @csrf
-                                <div class="form-group">
-                                    <label for="nama">Nama</label>
-                                    <input type="text" class="form-control" id="nama"
-                                        placeholder='Nama Lengkap' name='nama'
-                                        value='{{$administratorNama}}'
-                                        required />
-                                </div>
                                 <div class="row">
-                                    <div class="form-group col-lg-4">
-                                        <label for="telepon">Telepon</label>
-                                        <input type="text" class="form-control" id="telepon"
-                                            placeholder='Nomor Telepon' name='telepon'
-                                            value='{{$administratorTelepon}}'
-                                            required />
+                                    <div class="col-lg-4">
+                                        <label for="uploadFileProfile" data-toggle='tooltip' title='Klik gambar untuk mengupload Foto Profile baru'>
+                                            <img class="img-thumbnail w-100" src="{{(!empty($administratorFoto))? asset('admin_profile_pict/'.$administratorFoto) : ''}}"
+                                                alt="{{$administratorNama}}" onError='this.src="{{asset('img/icon.png')}}"'
+                                                id='foto' />
+                                            <input type="file" id="uploadFileProfile" name='uploadFileProfile'
+                                                onChange='_imgChanged(this)' data-preview='#foto' style='display:none;' />
+                                        </label>
                                     </div>
-                                    <div class="form-group col-lg-8">
-                                        <label for="email">Email</label>
-                                        <input type="email" class="form-control" id="email"
-                                            placeholder='Alamat Email' name='email'
-                                            value='{{$administratorEmail}}'
-                                            required />
+                                    <div class="col-lg-8">
+                                        <div class="form-group">
+                                            <label for="nama">Nama</label>
+                                            <input type="text" class="form-control" id="nama" placeholder='Nama Lengkap' name='nama' value='{{$administratorNama}}' required />
+                                        </div>
+                                        <div class="row">
+                                            <div class="form-group col-lg-4">
+                                                <label for="telepon">Telepon</label>
+                                                <input type="text" class="form-control" id="telepon" placeholder='Nomor Telepon' name='telepon' value='{{$administratorTelepon}}' required />
+                                            </div>
+                                            <div class="form-group col-lg-8">
+                                                <label for="email">Email</label>
+                                                <input type="email" class="form-control" id="email" placeholder='Alamat Email' name='email' value='{{$administratorEmail}}' required />
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="alamat">Alamat</label>
+                                            <textarea name="alamat" id="alamat" placeholder='Alamat Lengkap' class='form-control' required name='alamat'>{{$administratorAlamat}}</textarea>
+                                        </div>
+                                        <button class="btn btn-success" id="buttonSubmit" type='submit'>Update Profile</button>
                                     </div>
                                 </div>
-                                <div class="form-group">
-                                    <label for="alamat">Alamat</label>
-                                    <textarea name="alamat" id="alamat" placeholder='Alamat Lengkap'
-                                        class='form-control' required name='alamat'>{{$administratorAlamat}}</textarea>
-                                </div>
-                                <button class="btn btn-success" id="buttonSubmit" type='submit'>Update Profile</button>
                             </form>
                         </div>
                         <div class="tab-pane" id="gantiPassword">
+                            <h5 class='mb-1'>Form Ganti Password</h5>
+                            <p class="text-sm text-muted mb-4">Silahkan ganti password anda dengan mengisi form di bawah ini</p>
                             <form action="{{route('admin.profile.save-ganti-password')}}" id="formGantiPassword" method="post" onSubmit='_onFormGantiPasswordSubmit(this, event)'>
                                 @csrf
                                 <div class="form-group" id='passwordSaatIniContainer'>
                                     <label for="passwordSaatIni">Password Saat Ini</label>
                                     <div class="input-group">
-                                        <input type="password" class="form-control password" id="passwordSaatIni" 
-                                            placeholder='Password Saat Ini' name='passwordSaatIni' required />
+                                        <input type="password" class="form-control password" id="passwordSaatIni" placeholder='Password Saat Ini' name='passwordSaatIni' required />
                                         <div class="input-group-prepend">
                                             <div class="input-group-text cp" onClick='togglePassword(this, "#passwordSaatIniContainer")'>
                                                 <span class="password-icon fa fa-eye"></span>
@@ -98,8 +102,7 @@
                                 <div class="form-group" id='passwordBaruContainer'>
                                     <label for="passwordBaru">Password Baru</label>
                                     <div class="input-group">
-                                        <input type="password" class="form-control password" id="passwordBaru" 
-                                            placeholder='Password Baru' name='passwordBaru' required />
+                                        <input type="password" class="form-control password" id="passwordBaru" placeholder='Password Baru' name='passwordBaru' required />
                                         <div class="input-group-prepend">
                                             <div class="input-group-text cp" onClick='togglePassword(this, "#passwordBaruContainer")'>
                                                 <span class="password-icon fa fa-eye"></span>
@@ -110,8 +113,7 @@
                                 <div class="form-group" id='konfirmasiPasswordBaruContainer'>
                                     <label for="konfirmasiPasswordBaru">Konfirmasi Password Baru</label>
                                     <div class="input-group">
-                                        <input type="password" class="form-control password" id="konfirmasiPasswordBaru" 
-                                            placeholder='Konfirmasi Password Baru' name='konfirmasiPasswordBaru' required />
+                                        <input type="password" class="form-control password" id="konfirmasiPasswordBaru" placeholder='Konfirmasi Password Baru' name='konfirmasiPasswordBaru' required />
                                         <div class="input-group-prepend">
                                             <div class="input-group-text cp" onClick='togglePassword(this, "#konfirmasiPasswordBaruContainer")'>
                                                 <span class="password-icon fa fa-eye"></span>
@@ -146,42 +148,42 @@
 <script language='Javascript'>
     $('[data-toggle=tooltip]').tooltip();
 
-    let _fotoProfile    =   $('#fotoProfile');
-    
-    async function _imgChanged(thisContext){
+    let _fotoProfile = $('#fotoProfile');
+
+    async function _imgChanged(thisContext) {
         await fileHandler(thisContext);
     }
 
-    async function _onFormProfilSubmit(thisContext, eventContext){
+    async function _onFormProfilSubmit(thisContext, eventContext) {
         eventContext.preventDefault();
-        
-        let _el     =   $(thisContext);
-        await submitForm(thisContext, async (responseFromServer) => {
-            let _status     =   responseFromServer.status;
-            let _message    =   responseFromServer.message;
 
-            let _swalTitle      =   `Update Profile`;
-            let _swalMessage    =   _message ?? ((_status)? 'Berhasil!' : 'Gagal');
-            let _swalType       =   (_status)? 'success' : 'error';
+        let _el = $(thisContext);
+        await submitForm(thisContext, async (responseFromServer) => {
+            let _status = responseFromServer.status;
+            let _message = responseFromServer.message;
+
+            let _swalTitle = `Update Profile`;
+            let _swalMessage = _message ?? ((_status) ? 'Berhasil!' : 'Gagal');
+            let _swalType = (_status) ? 'success' : 'error';
 
             await notifikasi(_swalTitle, _swalMessage, _swalType);
         });
     }
-    
-    async function _onFormGantiPasswordSubmit(thisContext, eventContext){
-        eventContext.preventDefault();
-        
-        let _el     =   $(thisContext);
-        await submitForm(thisContext, async (responseFromServer) => {
-            let _status     =   responseFromServer.status;
-            let _message    =   responseFromServer.message;
 
-            let _swalTitle      =   `Ganti Password`;
-            let _swalMessage    =   _message ?? ((_status)? 'Berhasil!' : 'Gagal');
-            let _swalType       =   (_status)? 'success' : 'error';
+    async function _onFormGantiPasswordSubmit(thisContext, eventContext) {
+        eventContext.preventDefault();
+
+        let _el = $(thisContext);
+        await submitForm(thisContext, async (responseFromServer) => {
+            let _status = responseFromServer.status;
+            let _message = responseFromServer.message;
+
+            let _swalTitle = `Ganti Password`;
+            let _swalMessage = _message ?? ((_status) ? 'Berhasil!' : 'Gagal');
+            let _swalType = (_status) ? 'success' : 'error';
 
             await notifikasi(_swalTitle, _swalMessage, _swalType);
-            if(_status){
+            if (_status) {
                 _el.trigger('reset');
             }
         });
